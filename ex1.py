3b90a4381b725dd9f7153a5e6e2d5843295319e7
John Li <44646119+GuojunLee001@users.noreply.github.com>
2019-01-12 23:01:44 +0800

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# 加入上面这行确保运行在运行中文时不会出错
+
+# 指令计算机按照指令输出，重点是引号可以是单引号也可以是双引号，但必须对应（注：引号是半角字符而非全角字符）；如果计算机提示报错，则按照提示进行检查。
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# 加分题
+# 让脚本再多打印一行
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# ‘\n’为换行，‘\t’是tab，‘\\’才是普通字符斜杠\
+print ('\n')
+
+# 让脚本只打印一行
+print "Hello World!"
+print "This is a line," 
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
